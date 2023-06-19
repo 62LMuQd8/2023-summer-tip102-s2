4001c7f96e4406e73c926cb24244c6d79f676d80
62LMuQd8 <>
2023-06-20 00:08:22 +0100

    add u4p2

---
+++ u4p2/index.js
@@ -0,0 +1,4 @@
+import { Lunch } from './lunch.js'
+console.log(Lunch.parseStudents([1,1,0,0], [0,1,0,1]));
+console.log(Lunch.parseStudents([1], [0]));
+console.log(Lunch.parseStudents([1,1,1,0,0,1], [1,0,0,0,1,1]));
---
+++ u4p2/lunch.js
@@ -0,0 +1,33 @@
+export class Lunch {
+    // students is queue
+    // lunches is stack
+    static parseStudents(s, l) {
+      // redefine arrays to easily work with in Javascript
+      let students = s;
+      let lunches = l.reverse();
+      // keep track of the number of students who are unable to eat
+      // each time a student returns to end of queue, a student is unable to eat
+      let numStudentsUnableToEat = 0;
+      // keep looping until all remaining students are unable to eat lunch
+      // this happens when numStudentsUnableToEat === students.length
+      while (numStudentsUnableToEat < students.length) {
+        // when student lunch preference and lunch available matches
+        if (students[0] === lunches[lunches.length - 1]) {
+          // then remove lunch from stack, student from queue
+          lunches.pop();
+          students.shift();
+          // and reset number of students unable to eat
+          // b/c we are in effect looking at new students and new lunches
+          numStudentsUnableToEat = 0;
+  
+        // otherwise student returns to end of queue
+        // and number of students unable to eat is incremented
+        } else {
+          students.push(students.shift());
+          numStudentsUnableToEat++;
+        }
+      }
+      // return number of students unable to eat lunch
+      return numStudentsUnableToEat;
+    }
+  }
