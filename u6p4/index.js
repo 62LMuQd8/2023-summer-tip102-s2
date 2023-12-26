4e855385b3bc562e383083acd673851304bd7250
62LMuQd8 <>
2023-12-26 03:16:30 +0000

    clean up code

--- u6p4/index.js
+++ u6p4/index.js
@@ -4,12 +4,12 @@ import { Node } from "./node.js";
 console.log(TreeService.findDiameter(getRoot1()));
 console.log(TreeService.findDiameter(getRoot2()));
 
-function getRoot1(arr) {
+function getRoot1() {
     // [1,2,3,4,5]
     return new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3));
 }
 
-function getRoot2(arr) {
+function getRoot2() {
     // [1,2]
     return new Node(1, new Node(2));
 }
